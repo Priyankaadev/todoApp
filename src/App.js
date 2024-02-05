387015699b999aa9fe16f0892db94847496c7173
Priyankaadev <priyaaswal79@gmail.com>
2024-02-05 22:08:24 +0530

    event, rendering and commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Todo from './Todo';
 function App() {
+  const todos = [{data:'todo1', id: 312}, {data:'todo2', id: 313}, {data:'todo3', id: 314}];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Todo  todoList ={todos}/>
     </div>
   );
 }
---
+++ src/TodoItem.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import './TodoItem.css'
+function handleClick( todo){
+    alert( `${todo} Clicked`)
+}
+function TodoItem( props){
+const [isFinished, setIsFinished] = useState(false);
+    return (
+        <div>
+            <li onClick={()=> handleClick(props.todo.data)}> <span className={(isFinished)? 'todo-done':'todo-notdone'}>{props.todo.data}</span>
+            </li>
+            <button onClick={()=> setIsFinished(!isFinished)}>{(isFinished)? 'ReDo' : 'Done'}</button>
+        </div>
+    );
+}
+
+export default TodoItem;
